1e9e6eb77ce2c1125b5cb52fa7633bdf1d7dd476
Erika <erika.paige.handley@gmail.com>
2020-06-15 13:48:29 -0700

    added passort packages

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express')
-const app = express()
+const { join } = require('path')
+const passport = require('passport')
+const { Strategy } = require('passport-local')
+const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
 
+const app = express()
+const { User } = require('./models')
+
+// express middleware
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new Strategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.use(require('./routes'))
 
